a26b716ff77f4b26424bdbd776c23c512b491656
Papa Seydou WANE <129470066+SeydouWane@users.noreply.github.com>
2025-12-04 16:04:13 +0000

    Format varia.py for consistency

--- app/models/varia.py
+++ app/models/varia.py
@@ -2,8 +2,7 @@ from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional, List, TYPE_CHECKING
 from datetime import datetime
 from app.models.abstract_model import AbstractModel
-# Un Varia peut être ACTIF ou INACTIF, par exemple (ou utiliser un enum simple si nécessaire)
-# from app.models.enums import VariaStatus 
+
 
 if TYPE_CHECKING:
     from app.models.manuscript import Manuscript
@@ -18,3 +17,4 @@ class Varia(AbstractModel, table=True):
     
     is_active: bool = Field(default=True, nullable=False)
     manuscripts: List["Manuscript"] = Relationship(back_populates="varia")
+
